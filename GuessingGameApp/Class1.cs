1377d8f7e2b4a231d7835ec0a2e2afe792ca0590
JacobMason83 <jmason062083@gmail.com>
2021-06-30 14:26:30 -0400

    Add project files.

---
+++ GuessingGameApp/Class1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace GuessingGameApp
+{
+    public class Class1
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+
+            int winNum = r.Next(0, 100);
+
+            bool win = false;
+
+            do
+            {
+                Console.Write("Guess a number between 0 and 100");
+                string s = Console.ReadLine();
+
+                int i = int.Parse(s);
+                if (i > winNum)
+                {
+                    Console.WriteLine("Too High!! Go Below and try try again");
+                }
+                else if (i < winNum)
+                {
+                    Console.WriteLine("Your too Low!!! Go Higher my dude!!");
+                }
+                else if (i == winNum)
+                {
+                    Console.WriteLine("You Did it nice job!!!! Winner Winner Chicken Dinner ");
+                    win = true;
+                }
+            } while (win == false);
+        }
+    }
+};
